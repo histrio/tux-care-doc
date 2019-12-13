05915c6cca3391388cde6642b08041c6f49d1927
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2019-12-13 12:40:51 +0300

    Move to production domain docs.cln.cloudlinux.com

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ yarn docs:build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-echo 'docs-dev.cln.cloudlinux.com' > CNAME
+echo 'docs.cln.cloudlinux.com' > CNAME
 
 git config --global user.email "ntelepenin@cloudlinux.com"
 git config --global user.name "circle-ci"
